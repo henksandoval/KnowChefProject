d995bb57379b7df8e7ad7a2b3fd0c354752a3104
Henk Sandoval <henk.sandoval@outlook.com>
2021-12-05 14:08:11 +0100

    Changing end delimiter in Chef.Shared.Test

--- NetCore/Tests/Chef.Shared.Test/Utilities/ValueObjectTest/Models/ValueObjectModel.cs
+++ NetCore/Tests/Chef.Shared.Test/Utilities/ValueObjectTest/Models/ValueObjectModel.cs
@@ -1,26 +1,26 @@
 ﻿namespace Chef.Shared.Test.Utilities.ValueObjectTest.Models
 {
-    using Chef.Shared.Utilities.Value;
+	using Chef.Shared.Utilities.Value;
 
-    internal class ValueObjectModel : ValueObject
-    {
-        public ValueObjectModel() { }
+	internal class ValueObjectModel : ValueObject
+	{
+		public ValueObjectModel() { }
 
-        public ValueObjectModel(int nonPublicValue)
-        {
-            ProtectedProperty = nonPublicValue;
-            PrivateProperty = nonPublicValue;
-            privateField = nonPublicValue;
-            protectedField = nonPublicValue;
-        }
+		public ValueObjectModel(int nonPublicValue)
+		{
+			ProtectedProperty = nonPublicValue;
+			PrivateProperty = nonPublicValue;
+			privateField = nonPublicValue;
+			protectedField = nonPublicValue;
+		}
 
-        public string Property1 { get; set; }
-        public int Property2 { get; set; }
-        public int Field;
-        protected int protectedField;
-        private readonly int privateField;
+		public string Property1 { get; set; }
+		public int Property2 { get; set; }
+		public int Field;
+		protected int protectedField;
+		private readonly int privateField;
 
-        protected int ProtectedProperty { get; set; }
+		protected int ProtectedProperty { get; set; }
 #pragma warning disable IDE0052 // Remove unread private members
 		private int PrivateProperty { get; set; }
 #pragma warning restore IDE0052 // Remove unread private members
